2295ec2078b92412cafebc12f9825e4273aa69f0
francisthebasilisk <snorkysnark@gmail.com>
2021-04-09 11:54:49 +0300

    Comments in the example script

--- example/script/main.py
+++ example/script/main.py
@@ -12,6 +12,9 @@ async def try_download(id, url):
 async def page_task(id, url, out):
     await out.send(await try_download(id, url))
 
+# Main method, called by the framework
+# Return rows with out.send(Ok(value1, value2, value3, ...))
+# Return errors with out.send(Err("Error message"))
 async def run(input_rows, out):
     async with trio.open_nursery() as nurs:
         for (id, url) in input_rows:
