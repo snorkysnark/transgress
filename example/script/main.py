031ca7de7e5140d56200528dd30f55acdf8485cc
francisthebasilisk <snorkysnark@gmail.com>
2021-04-09 12:23:40 +0300

    add missing parenthesis in exaple script's comment

--- example/script/main.py
+++ example/script/main.py
@@ -13,7 +13,7 @@ async def page_task(id, url, out):
     await out.send(await try_download(id, url))
 
 # Main method, called by the framework
-# Return rows with out.send(Ok(value1, value2, value3, ...))
+# Return rows with out.send(Ok((value1, value2, value3, ...)))
 # Return errors with out.send(Err("Error message"))
 async def run(input_rows, out):
     async with trio.open_nursery() as nurs:
